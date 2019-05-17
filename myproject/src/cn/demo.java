28279fa8e7086aaf5d96a2e4582714e7df5fe993
yjhisbest <1411425707@qq.com>
2019-05-18 01:50:37 +0800

    add demo6

--- myproject/src/cn/demo.java
+++ myproject/src/cn/demo.java
@@ -4,4 +4,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("aaa");
     }
+    public static void test() {
+        System.out.println("test");
+    }
 }

6721ce93cb39c4d1c22e415239365bd07a5c809d
yjhisbest <1411425707@qq.com>
2019-05-17 19:31:17 +0800

    add demo2

--- github/demo/src/cn/demo/Demo.java
+++ github/demo/src/cn/demo/Demo.java
@@ -4,6 +4,5 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("bbb");
-        System.out.println("dev s");
     }
 }
--- github/demo/src/cn/demo/Demo2.java
+++
@@ -1,7 +0,0 @@
-package cn.demo;
-
-public class Demo2 {
-    public static void main(String[] args) {
-        System.out.println(1111);
-    }
-}

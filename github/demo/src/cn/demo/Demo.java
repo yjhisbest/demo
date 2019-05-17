30f757b97a0e3e704e240d040764c8fa02894b95
yjhisbest <1411425707@qq.com>
2019-05-17 20:19:25 +0800

    update demo

--- github/demo/src/cn/demo/Demo.java
+++ github/demo/src/cn/demo/Demo.java
@@ -2,7 +2,7 @@ package cn.demo;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("a123421313aa");
+        System.out.println("a123421313aanmf,sanm,fsanm,fsam,fa");
         System.out.println("bbb");
         System.out.println("push ccc");
     }

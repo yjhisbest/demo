387a891f9922f7d00a0a27b5e4a54b879f87d1fd
yjhisbest <1411425707@qq.com>
2019-05-22 09:33:24 +0800

    add dev

---
+++ myproject/src/cn/demo7.java
@@ -0,0 +1,11 @@
+package cn;
+
+public class demo7 {
+    public static void main(String[] args) {
+        System.out.println(77);
+    }
+
+    public static void test222(){
+        //todo
+    }
+}
